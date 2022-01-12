519c667329066d91924c6bdb4dbbb8152621f1f0
AndreSecco <andrefigueiro5@gmail.com>
2022-01-12 14:58:49 -0300

    Create ambiente.js

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+/*
+while(c <= 6){
+    console.log(`A contagem está em ${c}`)
+    c++
+}*/
+
+do{
+    console.log(`A contagem está em ${c}`)
+    c++
+}while(c <=6)
+
+
+
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+*/
